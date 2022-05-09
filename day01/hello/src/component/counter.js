ad04e4ee066be51e7f0020eeb02d74b1ab2d3f97
Seyoung Hong <maiorem00@gmail.com>
2022-05-09 21:43:51 +0900

    usestate function

--- day01/hello/src/component/counter.js
+++ day01/hello/src/component/counter.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 //반드시 대문자.. 소문자는 인식을 못함
 function Counter(props) {
+  //공통변수
+  //변수와 변수의 값을 바꿀 수 있는 함수 두개를 짝궁으로 만든다
+  //useState를 통해서 변수 초기화
+  const [count, setCount] = useState(10);
+
+  //함수의 주소를 저장하는 변수를 만든다
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
   let titleStyle = {
     fontSize: "23px",
     color: "red",
@@ -14,6 +28,13 @@ function Counter(props) {
       >
         This is counter2
       </h1>
+      <h1>현재 카운트 : {count}</h1>
+      <button type="button" onClick={increase}>
+        증가
+      </button>
+      <button type="button" onClick={decrease}>
+        감소
+      </button>
     </div>
   );
 }
